3092c62d005704389a1198b66ac52aec4300d8ef
CoconutTrebuchet <niraveati@outlook.com>
2024-05-03 22:21:55 -0400

    update bot.py and standalone_bot.py

--- bot.py
+++ bot.py
@@ -22,8 +22,15 @@ API_URL = "https://cosmo-api-six.vercel.app/"
 API_NEW = "https://api.cosmoship.duckdns.org/"
 
 # UNCOMMENT EACH OF THE DB COMMANDS BEFORE SUBMITTING PULL REQUEST
+
+# BOT_PATH = "/home/astrod/Desktop/Bots/cosmoteer-com/"
+BOT_PATH = "/Users/nirav/Documents/GitHub/cosmoteer-com" # for the love of god please remove this one especially
+# db = fight_db.FightDB(db_name=BOT_PATH+"test.db")
+# #db = fight_db.FightDB()
+
 # db = fight_db.FightDB(db_name="/home/astrod/Desktop/Bots/cosmoteer-com/test.db")
 #db = fight_db.FightDB()
+
 
 
 intents = discord.Intents.default()
@@ -31,7 +38,7 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 short_version_text="Made by LunastroD, Aug 2023 - Sep 2023"
-version_text=short_version_text+", for the Excelsior discord server and the Cosmoteer community :3\n- Check out the source code at <https://github.com/lunastrod/cosmoteer-com>\n- Thanks to Poney, Coconut Trebuchet and jun(0) for their help"
+version_text=short_version_text+", for the Excelsior discord server and the Cosmoteer community :3\n- Check out the source code at <https://github.com/lunastrod/cosmoteer-com>\n- Thanks to Poney, CoconutTrebuchet and jun(0) for their help"
 help_text=version_text+"""
 /ping: responds with the bot's latency
 
@@ -50,6 +57,34 @@ parameters:
 common questions:
 the bot considers the lateral thrust of each thruster
 the bot doesn't work with modded parts
+"""
+
+db_help_text = f"""/version: """+version_text+"""
+
+The Cosmoteer Design Tools bot has a database of every known multiplayer elimination archetype and their matchups that can be contributed to by anybody.
+Below is a list of commands that are used to access and contribute to the database.
+
+/help: Shows this message
+
+/db_list_ships: Lists every single ship type in the bot's archetype database. A handy reference for other db commands.
+
+/db_add_ships: Adds a new ship to the database.
+
+/db_rename_ship: Changes the existing name of a ship to a different specified one.
+
+/db_scoreboard: Shows the win/loss/draw ratio for each ship in the database based on matchups.
+
+/db_get_matchups: Lists each matchup (win, loss, draw) of a specified ship from the database.
+
+/db_get_unknown_matchups: Lists each matchup that has no votes from a specified ship from the database.
+
+/db_add_fight: Add a new matchup between 2 specified ship in the database.
+
+/db_remove_fight: Removes a matchup between 2 specified ship in the database.
+
+/db_simulate_fight: Simulates a fight between 2 specified ship, based on the listed matchup in the database.
+
+/db_export_csv: Exports the entire database to a .csv file.
 """
 
 @client.event
@@ -325,13 +360,14 @@ async def help(interaction: discord.Interaction):
     """
     print(dt.now(),"help command received")
     # Defer the initial response to prevent timeouts
-    await interaction.response.defer()
-    
-    # Generate the legend image for the command
-    #center_of_mass.draw_legend("legend.png")
-    
-    # Send the help text along with the legend image as a file
-    await interaction.followup.send(help_text, file=discord.File("legend.png"))
+    try:
+        await interaction.response.defer()
+        
+        # Send the help text along with the legend image as a file
+        await interaction.followup.send(help_text, file=discord.File(BOT_PATH+"legend.png"))
+    except Exception as e:
+        print(dt.now(),"Error:",e)
+        await interaction.followup.send("Error:"+str(e))
     print(dt.now(),"help command sent")
 
 @tree.command(name="elim_rps", description='play rock-paper-scissors, but with elimination archtypes!')
